727842f3b1e2c5b8c8bec8971af44f6ab5748894
xi4oz3ro <xi4oz3ro@qq.com>
2013-07-12 11:05:27 -0700

    1

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+	
+	define('APP_NAME', 'zhihu');
+	define('APP_PATH', './zhihu/');
+	define('APP_DEBUG', true);
+	//define('SAE_RUNTIME',true);
+	//define('ENGINE_NAME','sae');
+	require './ThinkPHP/ThinkPHP.php';
+
+ ?>
